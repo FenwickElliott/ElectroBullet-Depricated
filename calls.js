7bc2e47c45270cb7e53a4e99363b4e88cd0245d3
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-09-20 17:58:44 -0400

    refactoring

--- calls.js
+++ calls.js
@@ -1,22 +1,25 @@
 const Secrets = require('./secrets')
-const base_url = 'https://api.pushbullet.com/';
+const base_url = 'https://api.pushbullet.com/v2/';
 const jsonfile = require('jsonfile')
 const fetch = require('node-fetch');
 
-
 function get (endpoint, target){
-    console.log(base_url + endpoint);
-    target = './'+ target +'.json';
-    console.log(target);
     fetch(base_url + endpoint, {
         headers: {"Access-Token": Secrets.apiKey}
     })
     .then(res => res.json())
     .then(function(json){
         console.log(json);
-        jsonfile.writeFile(target, json, err => console.log(err));
+        jsonfile.writeFile('./db/'+ target +'.json', json, err => console.log(err));
         return;
     });
 }
 
-get('/v2/permanents/' + Secrets.deviceIden + '_threads', 'perms')
+// Get all SMS threads
+get('permanents/' + Secrets.deviceIden + '_threads', 'threads');
+
+// Get user info
+get('users/me', 'user_info');
+
+// Get devices
+get('devices', 'devices');
--- socket.js
+++ socket.js
@@ -5,12 +5,21 @@ var messages = document.getElementById('messages');
 var status = document.getElementById('status');
 var open = false;
 
+function openSocket (){
+    websocket = new WebSocket('wss://stream.pushbullet.com/websocket/' + Secrets.apiKey);
+    websocket.onmessage = function(e){
+        messages.innerHTML += "<p>" + e.data + "</p>";
+    }
+}
+
 switcher.onclick = function(){
     if (open){
+        websocket.close();
         open = false;
         document.getElementById('status').innerText = 'Closed';
         console.log('closing');
     } else {
+        openSocket();
         open = true;
         document.getElementById('status').innerText = 'Open';
         console.log('opening');
