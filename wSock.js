66ed4adbff6ae16e496f2912cd27972ee23b05f2
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-09-25 19:45:13 -0400

    websocket loading and running with notifications

--- wSock.js
+++ wSock.js
@@ -1,9 +1,8 @@
-const Secrets = require('./secrets');
 const websocket = new WebSocket('wss://stream.pushbullet.com/websocket/' + Secrets.apiKey);
 
 websocket.onmessage = function(e){
     let data = JSON.parse(e.data);
-    if (data.push.notifications) {
+    if (data.push && data.push.notifications) {
         data.push.notifications.forEach(function(n){
             new Notification(data.push.notifications[0].title, {
                 body: data.push.notifications[0].body
