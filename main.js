ec3297eee211d4fc7d9130a67d6e0266a4804adc
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-09-29 14:19:47 -0400

    icons added and all images moved from resources/ to assets/

--- main.js
+++ main.js
@@ -9,10 +9,13 @@ let win
 function createWindow () {
     // Create the browser window.
     win = new BrowserWindow({
+        // manage title bar
+        titleBarStyle: 'hidden',
         x: 0,
         y: 0,
         width: 960,
-        height: 600
+        height: 600,
+        icon: path.join(__dirname, 'assets/icons/png/png64x64.png')
     })
 
     // and load the index.html of the app.
